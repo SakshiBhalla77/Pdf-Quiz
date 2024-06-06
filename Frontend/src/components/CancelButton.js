de6d039c6d7f2ccbbd12ac059385b85b4fbb5dee
SakshiBhalla77 <sakshibhalla27@gmail.com>
2024-06-06 22:34:42 +0530

    submit button customised

--- Frontend/src/components/CancelButton.js
+++ Frontend/src/components/CancelButton.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const CancelButton = ({ onClick }) => {
-    return (
-        <button onClick={onClick} style={styles.button}>
-            Cancel
-        </button>
-    );
+  return (
+    <button onClick={onClick} style={styles.button}>
+      Cancel
+    </button>
+  );
 };
 
 const styles = {
-    button: {
-        marginLeft: '10px', // Add some spacing between buttons
-    },
+  button: {
+    marginLeft: "10px", // Add some spacing between buttons
+  },
 };
 
 export default CancelButton;
