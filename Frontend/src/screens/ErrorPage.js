8bc17f02863cb19258bc16a24ae4965fc029071a
SakshiBhalla77 <sakshibhalla27@gmail.com>
2024-06-17 00:21:21 +0530

    error page , image and font

--- Frontend/src/screens/ErrorPage.js
+++ Frontend/src/screens/ErrorPage.js
@@ -1,8 +1,21 @@
 import React from "react";
-const ErrorPage = () => {
+import "./ErrorPage.css"; // Import the CSS file for styling
+import cat from "../assets/cat.jpg"; // Import the image file
 
+const ErrorPage = () => {
   return (
-    <div>Hello</div>
+    <div className="error-container">
+      <div className="error-content">
+        <img src={cat} alt="Error Icon" className="error-icon" />
+        {/* <h1>404</h1> */}
+        <p className="page-not-found">Page Not Found</p>
+        <p>
+          Looks like you are lost! But don't let that stop you, head back to the
+          home page right away!
+        </p>
+        <button onClick={() => (window.location.href = "/")}>Go Back</button>
+      </div>
+    </div>
   );
 };
 
